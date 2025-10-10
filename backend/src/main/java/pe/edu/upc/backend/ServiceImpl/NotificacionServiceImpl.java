b09ba2d596a7bd70110796ad61dd647053e38251
George <u202318756@upc.edu.pe>
2025-10-10 15:37:21 -0700

    Agregar Implementacion de Servicio notificacion

---
+++ backend/src/main/java/pe/edu/upc/backend/ServiceImpl/NotificacionServiceImpl.java
@@ -0,0 +1,46 @@
+package pe.edu.upc.backend.ServiceImpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upc.backend.Entitie.Notificacion;
+import pe.edu.upc.backend.Repository.NotificacionRepository;
+import pe.edu.upc.backend.Service.NotificacionService;
+
+import java.util.List;
+
+@Service
+public class NotificacionServiceImpl implements NotificacionService {
+
+    @Autowired
+    private NotificacionRepository notificacionRepository;
+
+    @Override
+    public Notificacion add(Notificacion notificacion) {
+        return notificacionRepository.save(notificacion);
+    }
+
+    @Override
+    public List<Notificacion> findAll() {
+        return notificacionRepository.findAll();
+    }
+
+    @Override
+    public Notificacion edit(Notificacion notificacion) {
+        if (notificacionRepository.existsById(notificacion.getId())) {
+            return notificacionRepository.save(notificacion);
+        }
+        return null;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        if (notificacionRepository.existsById(id)) {
+            notificacionRepository.deleteById(id);
+        }
+    }
+
+    @Override
+    public List<Notificacion> findByUserId(Long userId) {
+        return notificacionRepository.findByUserId(userId);
+    }
+}
