f0ee4e194c13dcb0706644d2dc95d83e256fa973
George <u202318756@upc.edu.pe>
2025-11-30 22:20:12 -0300

    merge app-route.ts

--- frontend/src/app/app-module.ts
+++ frontend/src/app/app-module.ts
@@ -3,29 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { Login } from './components/login/login';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { provideNativeDateAdapter } from '@angular/material/core';
-<<<<<<< HEAD
-import { MaterialModule } from './modules/material/material-module';
-=======
 import { Login } from './components/login/login';
 import { Home } from './components/home/home';
 import { autorizacionInterceptor } from './interceptors/autorizacion-interceptor';
 import { Header } from './header/header';
->>>>>>> a609277653a612f3152676da2483e697db11e5dd
+import { MaterialModule } from './modules/material/material-module';
 
 @NgModule({
   declarations: [
     App,
-<<<<<<< HEAD
-    Login
-=======
     Login,
     Home,
     Header
->>>>>>> a609277653a612f3152676da2483e697db11e5dd
   ],
   imports: [
     BrowserModule,
@@ -38,16 +30,12 @@ import { Header } from './header/header';
   ],
   providers: [
     provideBrowserGlobalErrorListeners(),
-<<<<<<< HEAD
-    provideNativeDateAdapter()
-=======
 
     provideNativeDateAdapter(),
     provideHttpClient(
       withInterceptors([autorizacionInterceptor])
     )
     
->>>>>>> a609277653a612f3152676da2483e697db11e5dd
   ],
   bootstrap: [App]
 })
