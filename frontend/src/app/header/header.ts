6f1e4ff47145beb6f75bc17facfbb1e40785eb23
Faep28 <faep2823@gmail.com>
2025-12-02 18:35:51 -0500

    Actualizar header de los logueados

--- frontend/src/app/header/header.ts
+++ frontend/src/app/header/header.ts
@@ -10,7 +10,22 @@ import { Router } from '@angular/router';
 })
 export class Header {
 
+  isAdmin = false;
+  isUser  = false;
+
   constructor ( private userService: UserService, private router:Router ){}
+
+  ngOnInit(): void {
+    this.cargarRoles();
+  }
+
+  cargarRoles() {
+    const permisos = this.userService.getAuthorities() ?? ''; // "ROLE_ADMIN;ROLE_USER"
+
+    this.isAdmin = permisos.includes('ROLE_ADMIN');
+    // Considero user si tiene ROLE_USER o si es admin
+    this.isUser  = permisos.includes('ROLE_USER') || this.isAdmin;
+  }
 
   Logout(){
       this.userService.logout();
